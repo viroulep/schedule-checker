05c156f6c2a0e58f69dadaafa1389a00c8beb06d
Philippe Virouleau <philippe.44@gmail.com>
2020-04-05 22:05:35 +0200

    Load group simulator as wasm

--- src/App.js
+++ src/App.js
@@ -1,35 +1,124 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { Container, Header, Message, Icon } from 'semantic-ui-react';
+import { Button, Container, Header, Message, Icon } from 'semantic-ui-react';
 import Navigation from './Navigation/Navigation';
 
-const LoadingWasm = () => (
-  <Message icon info>
-    <Icon name='circle notched' loading />
-    <Message.Content>
-      <Message.Header>Just one second</Message.Header>
-      We are fetching that content for you.
-    </Message.Content>
-  </Message>
+import Simu from '@viroulep/group-simulator';
+
+const LoadingMessage = () => (
+  <>
+    <Message.Header>Loading required content</Message.Header>
+    Please wait while the page is loading a required module.
+  </>
 );
+
+const LoadedMessage = () => (
+  <>
+    Module loaded, enjoy the application :)
+  </>
+);
+
+const FailureMessage = () => (
+  <>
+    <p>
+      The loading of the module timed out.
+      It's likely that there was an error and that your browser does not support WebAssembly.
+    </p>
+
+    <p>
+      This module is necessary for this application, it is pointless to show you anything more than this error message at this point.
+      <br />
+      Feel free to check this website to make sure your browser is supported.
+    </p>
+    <Button
+      href="https://caniuse.com/#feat=wasm"
+      target="_blank"
+      secondary
+    >
+      Check browser compatibility
+    </Button>
+  </>
+);
+
+// Use dismissable with wasm: info loading, green loaded, red timeout!
+const LoadingWasm = ({
+  simulator,
+  loading
+}) => {
+  const [visible, setVisible] = useState(true);
+
+  return (
+    <>
+      {visible && (
+        <Message
+          icon
+          onDismiss={() => setVisible(false)}
+          color={simulator ? 'pink' : (loading ? 'teal' : 'red')}
+        >
+          {loading && (
+            <Icon name='circle notched' loading />
+          )}
+          <Message.Content>
+            {simulator ? (
+              <LoadedMessage />
+            ) : (loading ? (
+              <LoadingMessage />
+            ) : (
+              <FailureMessage />
+            ))}
+          </Message.Content>
+        </Message>
+      )}
+    </>
+  );
+};
 
 function App() {
   const [simulator, setSimulator] = useState(undefined);
-  if (!simulator)
-    setTimeout(() => setSimulator(true), 3000);
+  const [loading, setLoading] = useState(true);
+
+  const loadWasm = async () => {
+    const wasm = Simu(() => { setSimulator(wasm); setLoading(false) });
+  };
+
+  // Pass '[]' as a dependency, so that it's ran just once.
+  useEffect(() => {
+    loadWasm();
+    // Register a timeout on the loading process
+    setTimeout(() => setLoading(false), 3000);
+  }, []);
+
+  const doSomething = () => {
+    const times = new simulator.VectorTime();
+    console.log(times);
+    for (let i = 0; i < 12; i++) {
+      // people do 12s average
+      times.push_back(12);
+    }
+    console.log(simulator.getSetupProps());
+    const { Err, Value } = simulator.simuGroup("333", times, simulator.DEFAULT_MODEL);
+    console.log(simulator);
+    if (Err != simulator.ErrorKind.SUCCESS) {
+      console.log(simulator.errorMessage(Err));
+    } else {
+      console.log("Result of the simulation:");
+      const minutes = Math.floor(Value / 60);
+      const seconds = Value - minutes * 60;
+      console.log(`It lasted ${minutes} minutes and ${seconds} seconds.`);
+    }
+  }
 
   return (
     <Container>
-      {simulator ? (
+      <LoadingWasm simulator={simulator} loading={loading} />
+      {simulator && (
         <>
           <Navigation />
           <Header>
             Coucou
           </Header>
+          <Button primary onClick={doSomething} content="Test the thingy" />
         </>
-      ) : (
-        <LoadingWasm />
       )}
     </Container>
   );
--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'fomantic-ui-css/semantic.css';
 
+// NOTE:
+//import { Link } from 'react-router-dom'
+//import { Button } from 'semantic-ui-react'
+
+//// 💡 `to` prop is not handled in `Button` and will be passed to `Link` component
+//<Button as={Link} to="/home">
+  //To homepage
+//</Button>
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
