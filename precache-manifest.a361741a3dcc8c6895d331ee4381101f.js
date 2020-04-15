e242f9f35ba2220a24c031564f2c2a2a05e8ab8a
Philippe Virouleau <pvirouleau@quarkslab.com>
2020-04-15 21:43:58 +0200

    Updates

---
+++ precache-manifest.a361741a3dcc8c6895d331ee4381101f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4457a9214a4f6cbddf719a71fbd73580",
+    "url": "/schedule-checker/group-simulator.4457a921.wasm"
+  },
+  {
+    "revision": "077f620f62cb6036cbfcd8183f15f040",
+    "url": "/schedule-checker/index.html"
+  },
+  {
+    "revision": "0c6bc268d4154a36e9f2",
+    "url": "/schedule-checker/static/css/2.b91fe813.chunk.css"
+  },
+  {
+    "revision": "1cc127c4558af820a309",
+    "url": "/schedule-checker/static/css/main.6a44fc4d.chunk.css"
+  },
+  {
+    "revision": "0c6bc268d4154a36e9f2",
+    "url": "/schedule-checker/static/js/2.61f82827.chunk.js"
+  },
+  {
+    "revision": "d651ee430be7174996cabec478473789",
+    "url": "/schedule-checker/static/js/2.61f82827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cc127c4558af820a309",
+    "url": "/schedule-checker/static/js/main.edd7b543.chunk.js"
+  },
+  {
+    "revision": "093fa36e026064f66fdc",
+    "url": "/schedule-checker/static/js/runtime-main.6c3697af.js"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/schedule-checker/static/media/brand-icons.088a34f7.eot"
+  },
+  {
+    "revision": "0eb8cedcedefe0b04a200dc5c9602f4a",
+    "url": "/schedule-checker/static/media/brand-icons.0eb8cedc.svg"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/schedule-checker/static/media/brand-icons.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/schedule-checker/static/media/brand-icons.822d94f1.woff2"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/schedule-checker/static/media/brand-icons.f4920c94.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/schedule-checker/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/schedule-checker/static/media/icons.2aa6edf8.ttf"
+  },
+  {
+    "revision": "3719793fcebc92ff6642bf287db11b16",
+    "url": "/schedule-checker/static/media/icons.3719793f.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/schedule-checker/static/media/icons.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/schedule-checker/static/media/icons.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/schedule-checker/static/media/icons.f6121be5.woff2"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/schedule-checker/static/media/outline-icons.3ac49cb3.eot"
+  },
+  {
+    "revision": "9b4c53aa538b8e0c4def479a43a8acf1",
+    "url": "/schedule-checker/static/media/outline-icons.9b4c53aa.svg"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/schedule-checker/static/media/outline-icons.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/schedule-checker/static/media/outline-icons.a57bcf76.woff"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/schedule-checker/static/media/outline-icons.ece54318.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fd64464c176c73e5948a29f58790a62c.js"
+  "/schedule-checker/precache-manifest.a361741a3dcc8c6895d331ee4381101f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/schedule-checker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
