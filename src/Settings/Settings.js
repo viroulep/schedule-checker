00aabd41a8b4526c542d7ce40fe8ab9b69181a99
Philippe Virouleau <philippe.44@gmail.com>
2020-04-27 23:00:36 +0200

    Import/export simulator settings as yaml

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link, Router } from '@reach/router';
 import {
   Grid,
@@ -12,6 +12,7 @@ import {
 } from 'semantic-ui-react';
 import _ from 'lodash';
 import { asMap, SettingsDescription } from '@viroulep/group-simulator';
+import yaml from 'js-yaml';
 
 import {
   isPartiallyActive, isExactlyActive,
@@ -19,28 +20,58 @@ import {
 import './Settings.scss';
 import { storeConfig, loadStoredConfig, clearAndRefreshSettings } from '../utils';
 
+const exportAsYaml = (simulator) => {
+  const { mapSetup, mapModel, mapScrambling } = loadStoredConfig(simulator);
+  const yamlDump = yaml.safeDump({
+    setup: mapSetup,
+    model: mapModel,
+    scrambling: mapScrambling,
+  });
+  const blob = new Blob([yamlDump], {
+    type: 'application/x-yaml',
+  });
+  const blobURL = window.URL.createObjectURL(blob);
+  const tmp = document.createElement('a');
+  tmp.href = blobURL;
+  tmp.setAttribute('download', 'simulator-settings.yml');
+  document.body.appendChild(tmp);
+  tmp.click();
+};
+
 const DropdownMenu = ({
   saveAction,
   hasChanges,
-}) => (
-  <Dropdown item icon="cogs" className="right">
-    <Dropdown.Menu>
-      <Dropdown.Item
-        onClick={saveAction}
-        disabled={!hasChanges}
-      >
-        Save settings
-      </Dropdown.Item>
-      <Dropdown.Item onClick={clearAndRefreshSettings}>Reset settings to default</Dropdown.Item>
-    </Dropdown.Menu>
-  </Dropdown>
-);
-
-/* TODO: for import/export
-<Dropdown.Divider />
-  <Dropdown.Header>Export</Dropdown.Header>
-  <Dropdown.Item>Share</Dropdown.Item>
-  */
+  simulator,
+  onFileSelected,
+}) => {
+  const fileInput = useRef(null);
+  return (
+    <Dropdown item icon="cogs" className="right">
+      <Dropdown.Menu>
+        <Dropdown.Item
+          onClick={saveAction}
+          disabled={!hasChanges}
+        >
+          Save settings
+        </Dropdown.Item>
+        <Dropdown.Item onClick={clearAndRefreshSettings}>Reset settings to default</Dropdown.Item>
+        <Dropdown.Divider />
+        <Dropdown.Header>Settings as YAML</Dropdown.Header>
+        <Dropdown.Item onClick={() => fileInput.current.click()}>
+          Import
+          <input
+            type="file"
+            ref={fileInput}
+            accept=".yml,.yaml"
+            style={{ display: 'none' }}
+            onChange={(ev) => onFileSelected(ev, fileInput)}
+          />
+        </Dropdown.Item>
+        <Dropdown.Item onClick={() => exportAsYaml(simulator)}>Export</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  )
+};
 
 const SettingsPanel = ({
   options,
@@ -84,6 +115,41 @@ const SettingsPanel = ({
   );
 };
 
+const loadStoredAndSet = (
+  simulator, setSetup, setModel, setScrambling, setSaved,
+) => {
+  const { mapSetup, mapModel, mapScrambling } = loadStoredConfig(simulator);
+  setSetup(mapSetup);
+  setModel(mapModel);
+  setScrambling(mapScrambling);
+  const newConfig = {
+    setup: mapSetup,
+    model: mapModel,
+    scrambling: mapScrambling,
+  };
+  setSaved(newConfig);
+  storeConfig(newConfig);
+};
+
+const loadConfig = (
+  simulator, setup, model, scrambling,
+  setSetup, setModel, setScrambling, setSaved,
+) => {
+  // We let the simulator handle the validation, therefore we:
+  //   - clear the stored data
+  //   - load the submitted config
+  //   - load the stored config (which will return the - sanitized - config
+  //   set in the simulator)
+  //   - set the saved config.
+  storeConfig({});
+  simulator.loadConfig(
+    asMap(simulator.MapStringInt, setup),
+    asMap(simulator.MapStringInt, model),
+    asMap(simulator.MapStringInt, scrambling),
+  );
+  loadStoredAndSet(simulator, setSetup, setModel, setScrambling, setSaved);
+};
+
 const Settings = ({
   simulator,
 }) => {
@@ -93,27 +159,48 @@ const Settings = ({
   const [saved, setSaved] = useState({});
 
   useEffect(() => {
-    const { mapSetup, mapModel, mapScrambling } = loadStoredConfig(simulator);
-    setSetup(mapSetup);
-    setModel(mapModel);
-    setScrambling(mapScrambling);
-    setSaved({
-      setup: mapSetup,
-      model: mapModel,
-      scrambling: mapScrambling,
-    });
-  }, [simulator]);
-
-  const loadConfig = () => {
-    const newConfig = { setup, model, scrambling };
-    simulator.loadConfig(
-      asMap(simulator.MapStringInt, setup),
-      asMap(simulator.MapStringInt, model),
-      asMap(simulator.MapStringInt, scrambling),
+    loadStoredAndSet(
+      simulator, setSetup, setModel, setScrambling, setSaved,
     );
-    setSaved(newConfig);
-    storeConfig(newConfig);
+  }, [simulator, setSetup, setModel, setScrambling, setSaved]);
+
+  const saveAction = () => {
+    loadConfig(
+      simulator, setup, model, scrambling,
+      setSetup, setModel, setScrambling, setSaved,
+    );
   }
+
+  const [loadError, setLoadError] = useState(null);
+  const loadFromYaml = (event, fileInput) => {
+    setLoadError(null);
+    const importYamlString = (yamlString) => {
+      try {
+        const asObj = yaml.safeLoad(yamlString);
+        const newConfig = {
+          setup: {},
+          model: {},
+          scrambling: {},
+          ...asObj,
+        };
+        loadConfig(
+          simulator, newConfig.setup, newConfig.model, newConfig.scrambling,
+          setSetup, setModel, setScrambling, setSaved,
+        );
+      } catch (error) {
+        setLoadError(error.message);
+      }
+    }
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      importYamlString(e.target.result)
+      // Clear the input so that we can re-upload the same file!
+      /* eslint-disable no-param-reassign */
+      fileInput.current.value = ''
+    };
+    reader.onerror = (e) => setLoadError(`Couldn't load the YAML file (${e})`);
+    reader.readAsText(event.target.files[0]);
+  };
 
   const seHasChanges = !_.isEqual(saved.setup, setup);
   const mHasChanges = !_.isEqual(saved.model, model);
@@ -131,6 +218,12 @@ const Settings = ({
         <br />
         All times are expressed in seconds.
       </Message>
+      {loadError && (
+        <Message negative>
+          Error while loading the settings:
+          <pre>{loadError}</pre>
+        </Message>
+      )}
       <Menu attached="top" pointing color="violet">
         <Link to="" getProps={isExactlyActive}>
           Setup
@@ -148,8 +241,10 @@ const Settings = ({
           {scHasChanges ? '*' : ''}
         </Link>
         <DropdownMenu
-          saveAction={loadConfig}
+          simulator={simulator}
+          saveAction={saveAction}
           hasChanges={hasAnyChange}
+          onFileSelected={loadFromYaml}
         />
       </Menu>
       <Segment attached="bottom">
